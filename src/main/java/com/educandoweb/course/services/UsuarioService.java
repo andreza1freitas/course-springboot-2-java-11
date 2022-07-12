26f26ef99ecadbb4172f11e7585dcad1b86d9147
Andreza Freitas <andreza1freitas@gmail.com>
2022-07-12 17:17:13 -0300

    Exception handling - delete

--- src/main/java/com/educandoweb/course/services/UsuarioService.java
+++ src/main/java/com/educandoweb/course/services/UsuarioService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.course.entities.Usuario;
 import com.educandoweb.course.repositories.UsuarioRepository;
+import com.educandoweb.course.services.exceptions.DatabaseException;
 import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,13 @@ public class UsuarioService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	// verificar depois esse @suppresswarnings
